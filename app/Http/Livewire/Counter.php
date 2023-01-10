e4a239afc3e45670614e3ec2f69e20370f4d1afd
Pobitrodeb <pobitrodn@gmail.com>
2023-01-10 21:21:15 +0600

    Counter Application livewire

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -6,6 +6,17 @@ use Livewire\Component;
 
 class Counter extends Component
 {
+    public $value = 0;
+
+    public function increment()
+    {
+        $this->value++;
+    }
+
+    public function decreement()
+    {
+        $this->value--;
+    }
     public function render()
     {
         return view('livewire.counter');
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,7 @@
-<div>
-  <h1>Congratulations, Pobitro Debnath </h1>
+<div class="text-center">
+    <h1> Counter Application livewire </h1>
+
+    <button wire:click="increment" class="btn btn-danger"> + </button>
+    <h4> {{ $value }} </h4>
+    <button wire:click="decreement" class="btn btn-danger"> - </button>
 </div>
