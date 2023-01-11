0105ec181597e8acdd6507acc046bc9c0895c8d0
Pobitrodeb <pobitrodn@gmail.com>
2023-01-11 22:02:06 +0600

    Actions session complete with form submit data '

--- app/Http/Livewire/Form.php
+++ app/Http/Livewire/Form.php
@@ -7,6 +7,7 @@ use Livewire\Component;
 class Form extends Component
 {
    public $message = "Dumy Content";
+   public $submitForMessage = '';
     public function render()
     {
         return view('livewire.form');
@@ -14,5 +15,10 @@ class Form extends Component
     public function onChange()
     {
         $this->message = 'Congratulations, your are click button';
+
+    }
+
+    public function submitForm(){
+        $this->submitForMessage = 'Your are Form Submited';
     }
 }
--- resources/views/livewire/form.blade.php
+++ resources/views/livewire/form.blade.php
@@ -1,4 +1,13 @@
 <div>
+ <h2 class="my-2 text-danger"> Livewire Form Submit  </h2>
+
+<form  class="my-5" wire:submit.prevent="submitForm">
+    <input type="text" placeholder="Enter your name">
+    <button class="btn btn-primary"> Submit Form </button>
+    {{ $submitForMessage }}
+</form>
+
+
   <a class="btn btn-danger" wire:click="onChange"> Click for change content</a>
   {{ $message }}
 </div>
